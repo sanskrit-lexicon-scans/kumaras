2c380019176864940a2eec07182f5b6bfd475429
funderburkjim <funderburkjim@gmail.com>
2025-05-13 14:00:04 -0400

    Create app0, app1

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-kumaras  link target
+
+##  kumaras link target.
+
+Ref: https://github.com/sanskrit-lexicon/PWG/issues/146
+
+PDF source:
+https://download.digitale-sammlungen.de/BOOKS/download.pl?id=bsb10218794
+
+license: https://rightsstatements.org/page/NoC-NC/1.0/
+
+PDF title:
+Kumāra Sambhava,  ed. Adolph Stenzler, 1838
+
+## app1
+Provides access to pages based on 2 numbers:
+sarga (1-7), śloka
+
+https://sanskrit-lexicon-scans.github.io/kumaras/app1/?N,N
+
+## app0
+Provides access to pages based on one number,N, the internal page number.
+N is in range 3 to 108
+
+https://sanskrit-lexicon-scans.github.io/kumaras/app0/?N
+
---
+++ app0/pywork/index.js
@@ -0,0 +1,640 @@
+indexdata = 
+[
+ {
+  "page": 16,
+  "sarga": 1,
+  "ipage": 3,
+  "vp": "016"
+ },
+ {
+  "page": 17,
+  "sarga": 1,
+  "ipage": 4,
+  "vp": "017"
+ },
+ {
+  "page": 18,
+  "sarga": 1,
+  "ipage": 5,
+  "vp": "018"
+ },
+ {
+  "page": 19,
+  "sarga": 1,
+  "ipage": 6,
+  "vp": "019"
+ },
+ {
+  "page": 20,
+  "sarga": 1,
+  "ipage": 7,
+  "vp": "020"
+ },
+ {
+  "page": 21,
+  "sarga": 1,
+  "ipage": 8,
+  "vp": "021"
+ },
+ {
+  "page": 22,
+  "sarga": 1,
+  "ipage": 9,
+  "vp": "022"
+ },
+ {
+  "page": 23,
+  "sarga": 1,
+  "ipage": 10,
+  "vp": "023"
+ },
+ {
+  "page": 24,
+  "sarga": 1,
+  "ipage": 11,
+  "vp": "024"
+ },
+ {
+  "page": 25,
+  "sarga": 1,
+  "ipage": 12,
+  "vp": "025"
+ },
+ {
+  "page": 26,
+  "sarga": 1,
+  "ipage": 13,
+  "vp": "026"
+ },
+ {
+  "page": 27,
+  "sarga": 1,
+  "ipage": 14,
+  "vp": "027"
+ },
+ {
+  "page": 28,
+  "sarga": 1,
+  "ipage": 15,
+  "vp": "028"
+ },
+ {
+  "page": 29,
+  "sarga": 2,
+  "ipage": 16,
+  "vp": "029"
+ },
+ {
+  "page": 30,
+  "sarga": 2,
+  "ipage": 17,
+  "vp": "030"
+ },
+ {
+  "page": 31,
+  "sarga": 2,
+  "ipage": 18,
+  "vp": "031"
+ },
+ {
+  "page": 32,
+  "sarga": 2,
+  "ipage": 19,
+  "vp": "032"
+ },
+ {
+  "page": 33,
+  "sarga": 2,
+  "ipage": 20,
+  "vp": "033"
+ },
+ {
+  "page": 34,
+  "sarga": 2,
+  "ipage": 21,
+  "vp": "034"
+ },
+ {
+  "page": 35,
+  "sarga": 2,
+  "ipage": 22,
+  "vp": "035"
+ },
+ {
+  "page": 36,
+  "sarga": 2,
+  "ipage": 23,
+  "vp": "036"
+ },
+ {
+  "page": 37,
+  "sarga": 2,
+  "ipage": 24,
+  "vp": "037"
+ },
+ {
+  "page": 38,
+  "sarga": 2,
+  "ipage": 25,
+  "vp": "038"
+ },
+ {
+  "page": 39,
+  "sarga": 2,
+  "ipage": 26,
+  "vp": "039"
+ },
+ {
+  "page": 40,
+  "sarga": 2,
+  "ipage": 27,
+  "vp": "040"
+ },
+ {
+  "page": 41,
+  "sarga": 3,
+  "ipage": 28,
+  "vp": "041"
+ },
+ {
+  "page": 42,
+  "sarga": 3,
+  "ipage": 29,
+  "vp": "042"
+ },
+ {
+  "page": 43,
+  "sarga": 3,
+  "ipage": 30,
+  "vp": "043"
+ },
+ {
+  "page": 44,
+  "sarga": 3,
+  "ipage": 31,
+  "vp": "044"
+ },
+ {
+  "page": 45,
+  "sarga": 3,
+  "ipage": 32,
+  "vp": "045"
+ },
+ {
+  "page": 46,
+  "sarga": 3,
+  "ipage": 33,
+  "vp": "046"
+ },
+ {
+  "page": 47,
+  "sarga": 3,
+  "ipage": 34,
+  "vp": "047"
+ },
+ {
+  "page": 48,
+  "sarga": 3,
+  "ipage": 35,
+  "vp": "048"
+ },
+ {
+  "page": 49,
+  "sarga": 3,
+  "ipage": 36,
+  "vp": "049"
+ },
+ {
+  "page": 50,
+  "sarga": 3,
+  "ipage": 37,
+  "vp": "050"
+ },
+ {
+  "page": 51,
+  "sarga": 3,
+  "ipage": 38,
+  "vp": "051"
+ },
+ {
+  "page": 52,
+  "sarga": 3,
+  "ipage": 39,
+  "vp": "052"
+ },
+ {
+  "page": 53,
+  "sarga": 3,
+  "ipage": 40,
+  "vp": "053"
+ },
+ {
+  "page": 54,
+  "sarga": 3,
+  "ipage": 41,
+  "vp": "054"
+ },
+ {
+  "page": 55,
+  "sarga": 3,
+  "ipage": 42,
+  "vp": "055"
+ },
+ {
+  "page": 56,
+  "sarga": 3,
+  "ipage": 43,
+  "vp": "056"
+ },
+ {
+  "page": 57,
+  "sarga": 4,
+  "ipage": 44,
+  "vp": "057"
+ },
+ {
+  "page": 58,
+  "sarga": 4,
+  "ipage": 45,
+  "vp": "058"
+ },
+ {
+  "page": 59,
+  "sarga": 4,
+  "ipage": 46,
+  "vp": "059"
+ },
+ {
+  "page": 60,
+  "sarga": 4,
+  "ipage": 47,
+  "vp": "060"
+ },
+ {
+  "page": 61,
+  "sarga": 4,
+  "ipage": 48,
+  "vp": "061"
+ },
+ {
+  "page": 62,
+  "sarga": 4,
+  "ipage": 49,
+  "vp": "062"
+ },
+ {
+  "page": 63,
+  "sarga": 4,
+  "ipage": 50,
+  "vp": "063"
+ },
+ {
+  "page": 64,
+  "sarga": 4,
+  "ipage": 51,
+  "vp": "064"
+ },
+ {
+  "page": 65,
+  "sarga": 4,
+  "ipage": 52,
+  "vp": "065"
+ },
+ {
+  "page": 66,
+  "sarga": 4,
+  "ipage": 53,
+  "vp": "066"
+ },
+ {
+  "page": 67,
+  "sarga": 5,
+  "ipage": 54,
+  "vp": "067"
+ },
+ {
+  "page": 68,
+  "sarga": 5,
+  "ipage": 55,
+  "vp": "068"
+ },
+ {
+  "page": 69,
+  "sarga": 5,
+  "ipage": 56,
+  "vp": "069"
+ },
+ {
+  "page": 70,
+  "sarga": 5,
+  "ipage": 57,
+  "vp": "070"
+ },
+ {
+  "page": 71,
+  "sarga": 5,
+  "ipage": 58,
+  "vp": "071"
+ },
+ {
+  "page": 72,
+  "sarga": 5,
+  "ipage": 59,
+  "vp": "072"
+ },
+ {
+  "page": 73,
+  "sarga": 5,
+  "ipage": 60,
+  "vp": "073"
+ },
+ {
+  "page": 74,
+  "sarga": 5,
+  "ipage": 61,
+  "vp": "074"
+ },
+ {
+  "page": 75,
+  "sarga": 5,
+  "ipage": 62,
+  "vp": "075"
+ },
+ {
+  "page": 76,
+  "sarga": 5,
+  "ipage": 63,
+  "vp": "076"
+ },
+ {
+  "page": 77,
+  "sarga": 5,
+  "ipage": 64,
+  "vp": "077"
+ },
+ {
+  "page": 78,
+  "sarga": 5,
+  "ipage": 65,
+  "vp": "078"
+ },
+ {
+  "page": 79,
+  "sarga": 5,
+  "ipage": 66,
+  "vp": "079"
+ },
+ {
+  "page": 80,
+  "sarga": 5,
+  "ipage": 67,
+  "vp": "080"
+ },
+ {
+  "page": 81,
+  "sarga": 5,
+  "ipage": 68,
+  "vp": "081"
+ },
+ {
+  "page": 82,
+  "sarga": 5,
+  "ipage": 69,
+  "vp": "082"
+ },
+ {
+  "page": 83,
+  "sarga": 5,
+  "ipage": 70,
+  "vp": "083"
+ },
+ {
+  "page": 84,
+  "sarga": 5,
+  "ipage": 71,
+  "vp": "084"
+ },
+ {
+  "page": 85,
+  "sarga": 6,
+  "ipage": 72,
+  "vp": "085"
+ },
+ {
+  "page": 86,
+  "sarga": 6,
+  "ipage": 73,
+  "vp": "086"
+ },
+ {
+  "page": 87,
+  "sarga": 6,
+  "ipage": 74,
+  "vp": "087"
+ },
+ {
+  "page": 88,
+  "sarga": 6,
+  "ipage": 75,
+  "vp": "088"
+ },
+ {
+  "page": 89,
+  "sarga": 6,
+  "ipage": 76,
+  "vp": "089"
+ },
+ {
+  "page": 90,
+  "sarga": 6,
+  "ipage": 77,
+  "vp": "090"
+ },
+ {
+  "page": 91,
+  "sarga": 6,
+  "ipage": 78,
+  "vp": "091"
+ },
+ {
+  "page": 92,
+  "sarga": 6,
+  "ipage": 79,
+  "vp": "092"
+ },
+ {
+  "page": 93,
+  "sarga": 6,
+  "ipage": 80,
+  "vp": "093"
+ },
+ {
+  "page": 94,
+  "sarga": 6,
+  "ipage": 81,
+  "vp": "094"
+ },
+ {
+  "page": 95,
+  "sarga": 6,
+  "ipage": 82,
+  "vp": "095"
+ },
+ {
+  "page": 96,
+  "sarga": 6,
+  "ipage": 83,
+  "vp": "096"
+ },
+ {
+  "page": 97,
+  "sarga": 6,
+  "ipage": 84,
+  "vp": "097"
+ },
+ {
+  "page": 98,
+  "sarga": 6,
+  "ipage": 85,
+  "vp": "098"
+ },
+ {
+  "page": 99,
+  "sarga": 6,
+  "ipage": 86,
+  "vp": "099"
+ },
+ {
+  "page": 100,
+  "sarga": 6,
+  "ipage": 87,
+  "vp": "100"
+ },
+ {
+  "page": 101,
+  "sarga": 6,
+  "ipage": 88,
+  "vp": "101"
+ },
+ {
+  "page": 102,
+  "sarga": 7,
+  "ipage": 89,
+  "vp": "102"
+ },
+ {
+  "page": 103,
+  "sarga": 7,
+  "ipage": 90,
+  "vp": "103"
+ },
+ {
+  "page": 104,
+  "sarga": 7,
+  "ipage": 91,
+  "vp": "104"
+ },
+ {
+  "page": 105,
+  "sarga": 7,
+  "ipage": 92,
+  "vp": "105"
+ },
+ {
+  "page": 106,
+  "sarga": 7,
+  "ipage": 93,
+  "vp": "106"
+ },
+ {
+  "page": 107,
+  "sarga": 7,
+  "ipage": 94,
+  "vp": "107"
+ },
+ {
+  "page": 108,
+  "sarga": 7,
+  "ipage": 95,
+  "vp": "108"
+ },
+ {
+  "page": 109,
+  "sarga": 7,
+  "ipage": 96,
+  "vp": "109"
+ },
+ {
+  "page": 110,
+  "sarga": 7,
+  "ipage": 97,
+  "vp": "110"
+ },
+ {
+  "page": 111,
+  "sarga": 7,
+  "ipage": 98,
+  "vp": "111"
+ },
+ {
+  "page": 112,
+  "sarga": 7,
+  "ipage": 99,
+  "vp": "112"
+ },
+ {
+  "page": 113,
+  "sarga": 7,
+  "ipage": 100,
+  "vp": "113"
+ },
+ {
+  "page": 114,
+  "sarga": 7,
+  "ipage": 101,
+  "vp": "114"
+ },
+ {
+  "page": 115,
+  "sarga": 7,
+  "ipage": 102,
+  "vp": "115"
+ },
+ {
+  "page": 116,
+  "sarga": 7,
+  "ipage": 103,
+  "vp": "116"
+ },
+ {
+  "page": 117,
+  "sarga": 7,
+  "ipage": 104,
+  "vp": "117"
+ },
+ {
+  "page": 118,
+  "sarga": 7,
+  "ipage": 105,
+  "vp": "118"
+ },
+ {
+  "page": 119,
+  "sarga": 7,
+  "ipage": 106,
+  "vp": "119"
+ },
+ {
+  "page": 120,
+  "sarga": 7,
+  "ipage": 107,
+  "vp": "120"
+ },
+ {
+  "page": 121,
+  "sarga": 7,
+  "ipage": 108,
+  "vp": "121"
+ }
+]
+; // end of indexdata
